b4400d5e5bec33773ff527d3b5d0e94414f658d9
Rolly93 <129896901+Rolly93@users.noreply.github.com>
2024-09-14 17:30:14 +0000

    first 1day on Python

---
+++ Python-course/bonus1.py
@@ -0,0 +1,13 @@
+text = input("Enter a title: ")
+lenght = len(text)
+print("the length of the title:",lenght)
+
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes' 'No'  ,my_answer]
+answers = ['Yes', 'No', 'Yes', 'No', 'No']
+
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+
+
